97fa4caf02ca00e0fe859e92db35035b7ebde87a
Zmdora <35474182+Zmdora@users.noreply.github.com>
2023-07-03 13:26:27 +1000

    Add files via upload

---
+++ param.js
@@ -0,0 +1,8 @@
+//phosphene array size
+var width = 40; //400/20
+var height = 30; //300/20
+
+//phosphene half size
+var hsize = 3; //5
+//phosphene spacing should be larger than hsize * 2
+var spacing = 10; //10
